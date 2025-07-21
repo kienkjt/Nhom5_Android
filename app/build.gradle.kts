f669f0c6f692bbc908ee4014d1f651f2d8964bc7
trungkien <kienn7454@gmail.com>
2025-07-21 18:33:30 +0700

    merge(main): merge first commit in main branch

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,6 +57,8 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.firebase.crashlytics.buildtools)
+    implementation(libs.navigation.fragment)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
