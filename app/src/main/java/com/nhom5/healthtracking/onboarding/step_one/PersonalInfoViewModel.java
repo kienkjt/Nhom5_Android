729678a287fd6789573d00caaf8fa63880a8af03
hieunm3103 <hieunm3103@gmail.com>
2025-08-06 04:44:41 +0700

    feat: init onboarding activity with profile info fragment

---
+++ app/src/main/java/com/nhom5/healthtracking/onboarding/step_one/PersonalInfoViewModel.java
@@ -0,0 +1,7 @@
+package com.nhom5.healthtracking.onboarding.step_one;
+
+import androidx.lifecycle.ViewModel;
+
+public class PersonalInfoViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
