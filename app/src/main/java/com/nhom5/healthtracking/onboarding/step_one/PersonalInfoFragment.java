1c0f7e25c9843573111400ba049ae70a22329987
hieunm3103 <hieunm3103@gmail.com>
2025-08-06 04:44:41 +0700

    feat: init onboarding activity with profile info fragment

---
+++ app/src/main/java/com/nhom5/healthtracking/onboarding/step_one/PersonalInfoFragment.java
@@ -0,0 +1,38 @@
+package com.nhom5.healthtracking.onboarding.step_one;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.nhom5.healthtracking.R;
+
+public class PersonalInfoFragment extends Fragment {
+
+    private PersonalInfoViewModel mViewModel;
+
+    public static PersonalInfoFragment newInstance() {
+        return new PersonalInfoFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_personal_info, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(PersonalInfoViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
