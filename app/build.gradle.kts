9ce798f5dbbde9db5d1d23660dc62e8050c63d0b
hieunm3103 <hieunm3103@gmail.com>
2025-08-06 21:04:16 +0700

    chore: add firebase dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -38,11 +39,15 @@ dependencies {
     implementation(libs.lifecycle.viewmodel.ktx)
     val room_version = "2.7.2"
     val bcrypt_version = "0.4"
+    val firebase_version = "34.0.0"
 
     implementation("androidx.room:room-runtime:$room_version")
     annotationProcessor("androidx.room:room-compiler:$room_version")
    implementation("org.mindrot:jbcrypt:$bcrypt_version")
    implementation("androidx.security:security-crypto:1.1.0")
+    implementation(platform("com.google.firebase:firebase-bom:$firebase_version"))
+    implementation("com.google.firebase:firebase-auth")
+    implementation("com.google.firebase:firebase-database")
 
 
     implementation(libs.appcompat)
