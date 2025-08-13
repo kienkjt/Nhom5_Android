8ffad4b996708ae68f47e14ea47fb46f3ddc027c
FPTShop <duongvietanh1103@gmail.com>
2025-08-13 11:29:27 +0700

    sleep

---
+++ app/src/main/java/com/nhom5/healthtracking/sleep/SleepTracker.java
@@ -0,0 +1,26 @@
+package com.nhom5.healthtracking.sleep;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.nhom5.healthtracking.R;
+
+public class SleepTracker extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_sleep_tracker);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
